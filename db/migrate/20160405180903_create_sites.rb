d1464b47bf0dd837dee9e29c6b2449cad7b01982
agdavid <antonio.g.david@gmail.com>
2016-04-05 14:16:44 -0400

    add migration for user_sites

--- db/migrate/20160405180903_create_sites.rb
+++ db/migrate/20160405180903_create_sites.rb
@@ -1,4 +1,9 @@
 class CreateSites < ActiveRecord::Migration
   def change
+    create_table :sites do |t|
+      t.string :name
+      t.string :description
+      t.timestamps null: false
+    end
   end
 end
---
+++ db/migrate/20160405181521_create_user_sites.rb
@@ -0,0 +1,8 @@
+class CreateUserSites < ActiveRecord::Migration
+  def change
+    create_table :user_sites do |t|
+      t.integer :user_id
+      t.integer :site_id
+    end
+  end
+end
