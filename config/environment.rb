8fc477b3a35dc259c9269a04be26e041a399c4db
agdavid <antonio.g.david@gmail.com>
2016-04-05 11:45:37 -0400

    add config/environment to directory structure

---
+++ config/environment.rb
@@ -0,0 +1,12 @@
+ENV['SINATRA_ENV'] ||= "development"
+
+require 'bundler/setup' #require bundler; needed to load gems in Gemfile
+Bundler.require #command bundler to load gems in Gemfile 
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
+
+require_all 'app' #load all files in app (i.e., MVC files)
+require_all 'public' #load all files in public (i.e., CSS, JS, images)
